5e46574cde61fd52febac7525c5ed7cb95996327
Adolfo <fitox.metallica@gmail.com>
2023-12-30 09:27:40 -0600

    Catalogo listo -> version desarrollo

--- services/contents_services_streaming.py
+++ services/contents_services_streaming.py
@@ -1,24 +1,25 @@
 from flask import Flask, request, jsonify
- 
+from flask import render_template
+
 import json
 class ContentService:
     @staticmethod
     def obtener_catalogo():
         catalogo = [
-            {'id': '1', 'nombre': 'El Padrino', 'genero': 'Drama', 'anio': '1972' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino.mp4'},
-            {'id': '2', 'nombre': 'El Padrino II', 'genero': 'Drama', 'anio': '1974' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino II.mp4'},
-            {'id': '3', 'nombre': 'El Padrino III', 'genero': 'Drama', 'anio': '1990' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino III.mp4'},
-            {'id': '4', 'nombre': 'El Padrino IV', 'genero': 'Drama', 'anio': '1994' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino IV.mp4'},
-            {'id': '5', 'nombre': 'El Padrino V', 'genero': 'Drama', 'anio': '1998' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino V.mp4'},
-            {'id': '6', 'nombre': 'El Padrino VI', 'genero': 'Drama', 'anio': '2002' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino VI.mp4'},
-            {'id': '7', 'nombre': 'El Padrino VII', 'genero': 'Drama', 'anio': '2006' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino VII.mp4'},
-            {'id': '8', 'nombre': 'El Padrino VIII', 'genero': 'Drama', 'anio': '2010' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino VIII.mp4'},
-            {'id': '9', 'nombre': 'El Padrino IX', 'genero': 'Drama', 'anio': '2014' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino IX.mp4'},
-            {'id': '10', 'nombre': 'El Padrino X', 'genero': 'Drama', 'anio': '2018' ,'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino X.mp4'},
+            {'id': '1', 'nombre': 'El Padrino', 'genero': 'Drama', 'anio': '1972' , 'poster': 'padrino.png', 'ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino.mp4'},
+            {'id': '2', 'nombre': 'Batman Begins', 'genero': 'Accion', 'anio': '2005' ,'poster': 'batman.png','ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino II.mp4'},
+            {'id': '3', 'nombre': 'Memento', 'genero': 'Drama', 'anio': '2001' ,'poster': 'memento.png','ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino III.mp4'},
+            {'id': '4', 'nombre': 'La naranja Mecanica', 'genero': 'Drama', 'anio': '1971' ,'poster': 'naranjamecanica.png','ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino IV.mp4'},
+            {'id': '5', 'nombre': 'Oppenheimer', 'genero': 'Drama', 'anio': '2023' ,'poster': 'oppenheimer.png','ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino V.mp4'},
+            {'id': '6', 'nombre': 'The house that jack built', 'genero': 'Drama', 'anio': '2018' ,'poster': 'thehousethatjackbuilt.png','ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino VI.mp4'},
+            {'id': '7', 'nombre': 'The hunt', 'genero': 'Drama', 'anio': '2012' ,'poster': 'thehunt.png','ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino VII.mp4'},
+            {'id': '8', 'nombre': 'Vangogh en la puerta de la eternidad', 'genero': 'Drama', 'anio': '2018' ,'poster': 'vangoghmovie.png','ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino VIII.mp4'},
+            {'id': '9', 'nombre': 'Casino', 'genero': 'Crimen/Suspenso', 'anio': '1995' ,'poster': 'casino.png','ubicacion' : 'C:/Users/Usuario/Desktop/Proyecto-Streaming/archivos/El Padrino IX.mp4'},
+             
         ]
         
         
-        return jsonify({'catalogo': catalogo})
+        return render_template('catalogo.html', catalogo=catalogo)
     
     
     
